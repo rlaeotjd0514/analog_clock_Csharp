1198f12db2e83fcb336ddfd7781af8218a58734a
대성 김 <dskim0514@naver.com>
2018-09-09 11:14:42 +0900

    first_ver
    방학숙제로 만듬

---
+++ Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace analog_clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.설정ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.이별의아침에ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.약속의꽃을ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.장식하자ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ㄹㅇ씹명작ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.눈물흘렸다ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.키아ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.보기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.물론슈타게가ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.제일명작이긴함ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ㅇㅇㅈToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.만든놈ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.김머성ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.머어어어성ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.내가내이름머성ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.이라고불러도ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.괜찮다는데ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.왜그래여ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.난머성이가ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.좋다고ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.출처ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.없음ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clock_panel = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.설정ToolStripMenuItem,
+            this.보기ToolStripMenuItem,
+            this.만든놈ToolStripMenuItem,
+            this.출처ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(477, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 설정ToolStripMenuItem
+            // 
+            this.설정ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.이별의아침에ToolStripMenuItem});
+            this.설정ToolStripMenuItem.Name = "설정ToolStripMenuItem";
+            this.설정ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.설정ToolStripMenuItem.Text = "설정";
+            // 
+            // 이별의아침에ToolStripMenuItem
+            // 
+            this.이별의아침에ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.약속의꽃을ToolStripMenuItem});
+            this.이별의아침에ToolStripMenuItem.Name = "이별의아침에ToolStripMenuItem";
+            this.이별의아침에ToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.이별의아침에ToolStripMenuItem.Text = "이별의 아침에 ";
+            // 
+            // 약속의꽃을ToolStripMenuItem
+            // 
+            this.약속의꽃을ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.장식하자ToolStripMenuItem});
+            this.약속의꽃을ToolStripMenuItem.Name = "약속의꽃을ToolStripMenuItem";
+            this.약속의꽃을ToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.약속의꽃을ToolStripMenuItem.Text = "약속의 꽃을";
+            // 
+            // 장식하자ToolStripMenuItem
+            // 
+            this.장식하자ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ㄹㅇ씹명작ToolStripMenuItem});
+            this.장식하자ToolStripMenuItem.Name = "장식하자ToolStripMenuItem";
+            this.장식하자ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.장식하자ToolStripMenuItem.Text = "장식하자";
+            // 
+            // ㄹㅇ씹명작ToolStripMenuItem
+            // 
+            this.ㄹㅇ씹명작ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.눈물흘렸다ToolStripMenuItem});
+            this.ㄹㅇ씹명작ToolStripMenuItem.Name = "ㄹㅇ씹명작ToolStripMenuItem";
+            this.ㄹㅇ씹명작ToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.ㄹㅇ씹명작ToolStripMenuItem.Text = "ㄹㅇ 씹명작";
+            // 
+            // 눈물흘렸다ToolStripMenuItem
+            // 
+            this.눈물흘렸다ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.키아ToolStripMenuItem});
+            this.눈물흘렸다ToolStripMenuItem.Name = "눈물흘렸다ToolStripMenuItem";
+            this.눈물흘렸다ToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.눈물흘렸다ToolStripMenuItem.Text = "눈물 흘렸다 ";
+            // 
+            // 키아ToolStripMenuItem
+            // 
+            this.키아ToolStripMenuItem.Name = "키아ToolStripMenuItem";
+            this.키아ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.키아ToolStripMenuItem.Text = "키아";
+            // 
+            // 보기ToolStripMenuItem
+            // 
+            this.보기ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.물론슈타게가ToolStripMenuItem});
+            this.보기ToolStripMenuItem.Name = "보기ToolStripMenuItem";
+            this.보기ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.보기ToolStripMenuItem.Text = "보기";
+            // 
+            // 물론슈타게가ToolStripMenuItem
+            // 
+            this.물론슈타게가ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.제일명작이긴함ToolStripMenuItem});
+            this.물론슈타게가ToolStripMenuItem.Name = "물론슈타게가ToolStripMenuItem";
+            this.물론슈타게가ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.물론슈타게가ToolStripMenuItem.Text = "물론 슈타게가";
+            // 
+            // 제일명작이긴함ToolStripMenuItem
+            // 
+            this.제일명작이긴함ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ㅇㅇㅈToolStripMenuItem});
+            this.제일명작이긴함ToolStripMenuItem.Name = "제일명작이긴함ToolStripMenuItem";
+            this.제일명작이긴함ToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.제일명작이긴함ToolStripMenuItem.Text = "제일 명작이긴함";
+            // 
+            // ㅇㅇㅈToolStripMenuItem
+            // 
+            this.ㅇㅇㅈToolStripMenuItem.Name = "ㅇㅇㅈToolStripMenuItem";
+            this.ㅇㅇㅈToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.ㅇㅇㅈToolStripMenuItem.Text = "ㅇ ㅇㅈ";
+            // 
+            // 만든놈ToolStripMenuItem
+            // 
+            this.만든놈ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.김머성ToolStripMenuItem,
+            this.머어어어성ToolStripMenuItem,
+            this.내가내이름머성ToolStripMenuItem,
+            this.난머성이가ToolStripMenuItem});
+            this.만든놈ToolStripMenuItem.Name = "만든놈ToolStripMenuItem";
+            this.만든놈ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.만든놈ToolStripMenuItem.Text = "만든놈";
+            // 
+            // 김머성ToolStripMenuItem
+            // 
+            this.김머성ToolStripMenuItem.Name = "김머성ToolStripMenuItem";
+            this.김머성ToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.김머성ToolStripMenuItem.Text = "김머성";
+            // 
+            // 머어어어성ToolStripMenuItem
+            // 
+            this.머어어어성ToolStripMenuItem.Name = "머어어어성ToolStripMenuItem";
+            this.머어어어성ToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.머어어어성ToolStripMenuItem.Text = "머어어어성";
+            // 
+            // 내가내이름머성ToolStripMenuItem
+            // 
+            this.내가내이름머성ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.이라고불러도ToolStripMenuItem});
+            this.내가내이름머성ToolStripMenuItem.Name = "내가내이름머성ToolStripMenuItem";
+            this.내가내이름머성ToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.내가내이름머성ToolStripMenuItem.Text = "내가 내 이름 머성";
+            // 
+            // 이라고불러도ToolStripMenuItem
+            // 
+            this.이라고불러도ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.괜찮다는데ToolStripMenuItem});
+            this.이라고불러도ToolStripMenuItem.Name = "이라고불러도ToolStripMenuItem";
+            this.이라고불러도ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.이라고불러도ToolStripMenuItem.Text = "이라고 불러도";
+            // 
+            // 괜찮다는데ToolStripMenuItem
+            // 
+            this.괜찮다는데ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.왜그래여ToolStripMenuItem});
+            this.괜찮다는데ToolStripMenuItem.Name = "괜찮다는데ToolStripMenuItem";
+            this.괜찮다는데ToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.괜찮다는데ToolStripMenuItem.Text = "괜찮다는데";
+            // 
+            // 왜그래여ToolStripMenuItem
+            // 
+            this.왜그래여ToolStripMenuItem.Name = "왜그래여ToolStripMenuItem";
+            this.왜그래여ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.왜그래여ToolStripMenuItem.Text = "왜그래여";
+            // 
+            // 난머성이가ToolStripMenuItem
+            // 
+            this.난머성이가ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.좋다고ToolStripMenuItem});
+            this.난머성이가ToolStripMenuItem.Name = "난머성이가ToolStripMenuItem";
+            this.난머성이가ToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.난머성이가ToolStripMenuItem.Text = "난 머성이가";
+            // 
+            // 좋다고ToolStripMenuItem
+            // 
+            this.좋다고ToolStripMenuItem.Name = "좋다고ToolStripMenuItem";
+            this.좋다고ToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.좋다고ToolStripMenuItem.Text = "좋다고";
+            // 
+            // 출처ToolStripMenuItem
+            // 
+            this.출처ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.없음ToolStripMenuItem});
+            this.출처ToolStripMenuItem.Name = "출처ToolStripMenuItem";
+            this.출처ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.출처ToolStripMenuItem.Text = "출처";
+            // 
+            // 없음ToolStripMenuItem
+            // 
+            this.없음ToolStripMenuItem.Name = "없음ToolStripMenuItem";
+            this.없음ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.없음ToolStripMenuItem.Text = "없음";
+            // 
+            // clock_panel
+            // 
+            this.clock_panel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.clock_panel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("clock_panel.BackgroundImage")));
+            this.clock_panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.clock_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clock_panel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.clock_panel.Location = new System.Drawing.Point(0, 24);
+            this.clock_panel.Name = "clock_panel";
+            this.clock_panel.Size = new System.Drawing.Size(477, 432);
+            this.clock_panel.TabIndex = 1;
+            this.clock_panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(477, 456);
+            this.Controls.Add(this.clock_panel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 설정ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 보기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 만든놈ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 김머성ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 머어어어성ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 내가내이름머성ToolStripMenuItem;
+        private System.Windows.Forms.Panel clock_panel;
+        private System.Windows.Forms.ToolStripMenuItem 이별의아침에ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 약속의꽃을ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 장식하자ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ㄹㅇ씹명작ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 눈물흘렸다ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 키아ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 물론슈타게가ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 제일명작이긴함ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ㅇㅇㅈToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 이라고불러도ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 괜찮다는데ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 왜그래여ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 난머성이가ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 좋다고ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 출처ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 없음ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace analog_clock
+{
+    public partial class Form1 : Form
+    {
+        Graphics clock;
+        Point hour_center;
+        double radius = 12.00;
+        int hour_length = 5;
+        int min_length = 3;
+        int sec_length = 2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "머시계";
+            clock = this.clock_panel.CreateGraphics();
+            init_clock();
+            timer_setting();
+            
+        }
+
+        private void init_clock()
+        {
+            this.hour_center = new Point(this.clock_panel.Width / 2, this.clock_panel.Height / 2);
+            radius = clock_panel.Height / 2;
+            hour_length = (int)(radius * 0.6);
+            min_length = (int)(radius * 0.3);
+            sec_length = (int)(radius * 0.2);
+        }
+
+        private void timer_setting ()
+        {
+            System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+            timer1.Interval = 1000;
+            timer1.Tick += time_act;
+            timer1.Start();
+             
+            
+        }
+
+        private void time_act(object s, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            double hour_r = (now.Hour % 12.00 + now.Minute / 60.00) * 30 * Math.PI / 180.00;
+            double min_r = (now.Minute + now.Second / 60) * 6 * Math.PI / 180.00;
+            double sec_r = (now.Second) * 6 * Math.PI / 180;
+            clock_panel.Refresh();
+            draw_clock(hour_r, min_r, sec_r);
+        }
+
+        private void draw_clock(double h, double m, double s)
+        {
+            Point next_point;
+            Pen pen = new Pen(Brushes.Yellow, 10);
+            Rectangle r = new Rectangle(hour_center.X - (int)(radius / 2) - 100, hour_center.Y - (int)(radius / 2) - 100, (int)radius + 200, (int)radius + 200);
+            clock.DrawEllipse(pen, r);
+            next_point = draw_line((int)(hour_length * Math.Sin(h)), (int)(-hour_length * Math.Cos(h)), 0, 0, Brushes.Red, 8, hour_center.X, hour_center.Y);
+
+            pen = new Pen(Brushes.YellowGreen, 7);
+            r = new Rectangle(next_point.X - (int)(radius / 2), next_point.Y - (int)(radius / 2), (int)radius, (int)radius);
+            clock.DrawEllipse(pen, r);
+            next_point = draw_line((int)(min_length * Math.Sin(m)), (int)(-min_length * Math.Cos(m)),
+                0, 0,
+                Brushes.Green, 3,
+                next_point.X, next_point.Y);
+
+            pen = new Pen(Brushes.DeepSkyBlue, 5);
+            r = new Rectangle(next_point.X - 50, next_point.Y - 50, 100 , 100);
+            clock.DrawEllipse(pen, r);
+            draw_line((int)(sec_length * Math.Sin(s)), (int)(-sec_length * Math.Cos(s)),
+                0, 0,
+                Brushes.Blue, 2,
+                next_point.X, next_point.Y);
+        }
+
+        private Point draw_line(int x1, int y1, int x2, int y2, Brush sex, int thick, int cx, int cy)
+        {
+            Pen pen = new Pen(sex, thick);
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            Point p1 = new Point(x1 + cx, y1 + cy);
+            Point p2 = new Point(x2 + cx, y2 + cy);
+            clock.DrawLine(pen, p1, p2);
+            return p1;
+        }
+
+        
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
